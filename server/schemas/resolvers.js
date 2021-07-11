add04f9d63b26e3163b32009f2e80dac85660189
michealjwellis <mellis.eriecon@gmail.com>
2021-07-11 12:14:48 -0400

    starting 21.2 .5

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,5 +1,6 @@
 const { User, Thought } = require('../models');
 const { AuthenticationError } = require('apollo-server-express');
+const { signToken } = require('../utils/auth')
 
 const resolvers = {
     Query: {
@@ -27,8 +28,10 @@ const resolvers = {
     },
     Mutation: {
         addUser: async (parent, args) => {
-            const user = await User.create(args)
-            return user
+            const user = await User.create(args);
+            const token = signToken(user);
+
+            return { token, user };
         },
         login: async (parent, { email, password }) => {
             const user = await User.findOne({ email });
@@ -43,7 +46,8 @@ const resolvers = {
                 throw new AuthenticationError('Incorrect credentials');
             }
 
-            return user;
+            const token = signToken(user);
+            return { token, user };
         }
     }
 }
