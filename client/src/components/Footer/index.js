19392b12728d8d2dd8e5c5cc123e99cb1ec322fb
michealjwellis <mellis.eriecon@gmail.com>
2021-07-11 14:16:24 -0400

    starting 21.3.4 / code working

---
+++ client/src/components/Footer/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer className="w-100 mt-auto bg-secondary p-4">
+      <div className="container">&copy;2020 by Micheal Jw Ellis Sr.</div>
+    </footer>
+  );
+};
+
+export default Footer;
