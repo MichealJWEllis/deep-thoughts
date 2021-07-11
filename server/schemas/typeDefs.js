11daf9248e6f2c27e43c1686332ba46e63b4f149
michealjwellis <mellis.eriecon@gmail.com>
2021-07-11 13:46:44 -0400

    starting 21.3.1 / code working

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -36,9 +36,12 @@ type Thought {
     token: ID!
     user: User
 }
-  type Mutation {
-      login(email: String!, password: String!): Auth
-      addUser(username: String!, email: String!, password: String!): Auth
+type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    addThought(thoughtText: String!): Thought
+    addReaction(thoughtId: ID!, reactionBody: String!): Thought
+    addFriend(friendId: ID!): User
   }
   
 `;
