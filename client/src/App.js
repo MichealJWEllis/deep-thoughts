30999d6bc48f509ded0017e396f979f115469b2c
Micheal Ellis <76069321+MichealJWEllis@users.noreply.github.com>
2021-07-12 14:41:48 -0400

    starting 21.5.6 / code working

--- client/src/App.js
+++ client/src/App.js
@@ -11,13 +11,6 @@ import Profile from './pages/Profile';
 import Signup from './pages/Signup';
 import { setContext } from '@apollo/client/link/context';
 
-const httpLink = createHttpLink({
-  uri: '/graphql',
-});
-const client = new ApolloClient({
-  link: authLink.concat(httpLink),
-  cache: new InMemoryCache(),
-});
 const authLink = setContext((_, { headers }) => {
   const token = localStorage.getItem('id_token');
   return {
@@ -27,6 +20,15 @@ const authLink = setContext((_, { headers }) => {
     },
   };
 });
+const httpLink = createHttpLink({
+  uri: '/graphql',
+});
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
+
+
 
 function App() {
   return (
